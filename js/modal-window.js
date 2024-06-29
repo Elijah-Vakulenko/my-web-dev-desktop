df8275f94ee77758e1d4c09cc54c7fa4832f2c18
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-06-29 14:39:47 +0100

    lections menu added

--- js/modal-window.js
+++ js/modal-window.js
@@ -1,27 +1,42 @@
 const GoitFolder = document.querySelector('#goit-collection');
 const crossBtn = document.querySelector('#cross-icon');
-const window = document.querySelector('.mw-goit');
+const modalWindow = document.querySelector('.mw-goit');
 const overlay = document.querySelector('#goit-overlay');
 
 GoitFolder.addEventListener('click', windowOpen);
 crossBtn.addEventListener('click', windowClose);
 
 function windowOpen() {
-    window.classList.add('is-open');
-overlay.classList.remove('hidden');
+    modalWindow.classList.add('is-open');
+    overlay.classList.remove('hidden');
     setTimeout(() => overlay.classList.add('visible'), 10);
 }
 
 function windowClose() {
-     window.classList.remove('is-open');
-overlay.classList.remove('visible');
+    modalWindow.classList.remove('is-open');
+    overlay.classList.remove('visible');
     setTimeout(() => {
         overlay.classList.add('hidden');
-    })
+    }, 300); 
 }
 
 overlay.addEventListener('click', (event) => {
     if (event.target === overlay) {
         windowClose();
     }
+});
+
+const anchors = document.querySelectorAll('.window-menu a[href^="#"]');
+anchors.forEach(function (anchor) {
+    anchor.addEventListener('click', function (event) {
+        event.preventDefault(); 
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+
+            event.stopPropagation();
+        }
+    });
 });
